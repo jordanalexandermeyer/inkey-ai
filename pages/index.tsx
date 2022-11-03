d829836a07e1ca4d25f7dbb4642a9aa90732bc2b
Jordan Alexander Meyer <jordan@porter.finance>
2022-11-02 21:14:12 -0700

    added home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,59 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 
 const Home: NextPage = () => {
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    // put page into loading state
+
+    // prevents form from redirecting on submit
+    event.preventDefault()
+
+    // get text from event
+    const target = event.target as typeof event.target & {
+      text: { value: string }
+    }
+    const text = target.text
+
+    // send text to OpenAI to get essay outline
+    // after receiving outline from OpenAI, exit loading state
+    // redirect to new page putting outline in query (https://stackoverflow.com/questions/72221255/how-to-pass-data-from-one-page-to-another-page-in-next-js)
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Essay generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="flex w-full flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-3xl font-bold">Essay generator</h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+        <p className="mt-6 text-2xl">
+          Enter an essay prompt to generate a 5 paragraph essay
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <form
+          className="mt-6 flex w-full flex-1 flex-col items-center justify-center"
+          onSubmit={handleSubmit}
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+          <input
+            className="border border-gray-400 rounded w-full pt-2 p-2 text-2xl"
+            type="text"
+            placeholder="Describe the impact of..."
+            minLength={20}
+            required
+          />
+          <button
+            className="mt-6 bg-blue-700 text-white font-medium py-4 px-8 rounded text-xl"
+            type="submit"
+          >
+            Write my essay
+          </button>
+        </form>
+      </main>
     </div>
   )
 }
