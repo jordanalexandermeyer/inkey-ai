f79f21ad59452fdc2c0096f312cd5ca738d08d0c
Jordan Alexander Meyer <jordan@porter.finance>
2022-11-30 19:48:26 -0800

    removed role provider from app

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,15 +1,10 @@
 initializeFirebaseApp()
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { RoleProvider } from '../components/RoleProvider'
 import initializeFirebaseApp from '../lib/initializeFirebase'
 
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
-  return (
-    <RoleProvider>
-      <Component {...pageProps} />
-    </RoleProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
