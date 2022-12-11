e4560daa348f9af2f78bfbfc523d03ed38e0fb25
Jordan Alexander Meyer <jordan@porter.finance>
2022-12-10 17:59:38 -0800

    added amplitude and reddit pixel

---
+++ lib/initializeAmplitude.ts
@@ -0,0 +1,8 @@
+import * as amplitude from '@amplitude/analytics-browser'
+
+const initializeAmplitude = () => {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'production')
+    amplitude.init(process.env.AMPLITUDE_API_KEY || '')
+}
+
+export default initializeAmplitude
