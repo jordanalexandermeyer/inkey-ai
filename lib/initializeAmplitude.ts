db697b5024e9ef96bce75773f021ecc5c440f8fd
Jordan Alexander Meyer <jordan@porter.finance>
2022-12-11 02:20:03 -0800

    fixed amplitude api key variable name

--- lib/initializeAmplitude.ts
+++ lib/initializeAmplitude.ts
@@ -2,7 +2,7 @@ import * as amplitude from '@amplitude/analytics-browser'
 
 const initializeAmplitude = () => {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'production')
-    amplitude.init(process.env.AMPLITUDE_API_KEY || '')
+    amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY || '')
 }
 
 export default initializeAmplitude
