2457538bd5f2510b0abe86171479c0f2e8ec609f
Jordan Alexander Meyer <jordan@porter.finance>
2022-12-27 15:32:16 -0600

    added getDoc

--- pages/api/debug.page.ts
+++ pages/api/debug.page.ts
@@ -56,7 +56,7 @@ export default async function handler(request: Request, response: Response) {
   const db = getFirestore()
 
   const usageDetailsDocRef = doc(db, 'usage_details', userId)
-  // const docSnapshot = await getDoc(usageDetailsDocRef)
+  const docSnapshot = await getDoc(usageDetailsDocRef)
 
   // if (!docSnapshot.exists()) {
   //   console.error('User', userId, 'does not exist')
