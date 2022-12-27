c1678301bb79f6fc44e8c5a2d1a827fd745f4bda
Jordan Alexander Meyer <jordan@porter.finance>
2022-12-27 14:26:50 -0600

    updated debug page

--- pages/debug.page.tsx
+++ pages/debug.page.tsx
@@ -2,8 +2,18 @@ import { NextPage } from 'next'
 
 const DebugPage: NextPage = () => {
   const getDebug = async () => {
-    const response = await fetch('/api/debug')
-    console.log('success')
+    const response = await fetch('/api/debug', {
+      method: 'POST',
+      body: JSON.stringify({
+        message: 'what is the meaning of life?',
+        id: 'ask-inkey',
+        userId: 'EhAvXjddzoUYZ1vOMDBvg3c3bCi2',
+        inputs: { prompt: 'what is the meaning of life?' },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
 
   return (
