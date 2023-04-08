b56453a7041cc91620d454fb06a93d8ccd893367
jinal <jinal@lognllc.com>
2023-04-08 09:22:59 -0700

    Update _app.page.tsx

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -34,7 +34,7 @@ function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
             async
             src="https://www.googletagmanager.com/gtag/js?id=GTM-NPDFFWT"
           />
-          <Script>{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-JRR52Z158H');`}</Script>
+          <Script>{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','GTM-NPDFFWT');`}</Script>
           <Script>{`!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/"+key+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="FEfeA0XI98qeBP2G2vj3thnF2dyKOpqB";analytics.SNIPPET_VERSION="4.15.3";analytics.load("FEfeA0XI98qeBP2G2vj3thnF2dyKOpqB");analytics.page()}}();`}</Script>
         </>
       )}
